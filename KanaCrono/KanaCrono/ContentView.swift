ba6000ecd53beb029d42713c54fe75906c0319aa
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2020-11-28 23:36:43 +0100

    Proyecto nuevo

---
+++ KanaCrono/KanaCrono/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KanaCrono
+//
+//  Created by Ion Jaureguialzo Sarasola on 28/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KanaCrono/KanaCrono/KanaCronoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KanaCronoApp.swift
+//  KanaCrono
+//
+//  Created by Ion Jaureguialzo Sarasola on 28/11/20.
+//
+
+import SwiftUI
+
+@main
+struct KanaCronoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
