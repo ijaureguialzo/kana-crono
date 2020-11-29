bd0e7d842601cb2ece64f3ac8c9d07428f0f891f
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2020-11-29 11:24:56 +0100

    Modo horizontal

--- KanaCrono/KanaCrono/ContentView.swift
+++ KanaCrono/KanaCrono/ContentView.swift
@@ -23,88 +23,169 @@ struct ContentView: View {
     @State private var silabarioSeleccionado = Silabario.hiragana
     @State private var nivelSeleccionado = Nivel.basico
 
+    // REF: https://www.hackingwithswift.com/books/ios-swiftui/changing-a-views-layout-in-response-to-size-classes
+    @Environment(\.verticalSizeClass) var sizeClass
+
     // REF: https://www.hackingwithswift.com/quick-start/swiftui/how-to-use-a-timer-with-swiftui
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
 
     var body: some View {
 
-        VStack {
-
+        if sizeClass == .regular {
             VStack {
-                Picker("Silabario", selection: $silabarioSeleccionado) {
-                    ForEach(Silabario.allCases, id: \.self) { silabario in
-                        Text(silabario.rawValue.capitalized)
+                VStack {
+                    Picker("Silabario", selection: $silabarioSeleccionado) {
+                        ForEach(Silabario.allCases, id: \.self) { silabario in
+                            Text(silabario.rawValue.capitalized)
+                        }
+                    }
+
+                    Picker("Nivel", selection: $nivelSeleccionado) {
+                        Text("Básico").tag(Nivel.basico)
+                        Text("Ten-Ten").tag(Nivel.tenten)
+                        Text("Compuestos").tag(Nivel.compuestos)
                     }
                 }
+                    .pickerStyle(SegmentedPickerStyle())
+                    .padding(.horizontal, 40)
 
-                Picker("Nivel", selection: $nivelSeleccionado) {
-                    Text("Básico").tag(Nivel.basico)
-                    Text("Ten-Ten").tag(Nivel.tenten)
-                    Text("Compuestos").tag(Nivel.compuestos)
+                VStack {
+                    Text(etiqueta)
+                        .frame(minWidth: 300)
+                        .opacity(verKana ? 1 : 0)
+                        .font(.custom("Hiragino Mincho ProN W3", size: 144))
+                        .padding()
+                        .foregroundColor(Color(.label))
+                        .background(Color("Fondo"))
+                        .cornerRadius(10.0)
+
+                    Text(romaji)
+                        .opacity(verRomaji ? 1 : 0)
                 }
+                    .padding(.vertical, 20)
+
+                Divider()
+
+                HStack {
+                    Toggle(isOn: $verKana) {
+                        Text("Kana")
+                            .frame(maxWidth: .infinity, alignment: .trailing)
+                    }
+                    Toggle(isOn: $verRomaji) {
+                        Text("Romaji")
+                            .frame(maxWidth: .infinity, alignment: .trailing)
+                    }
+                    Toggle(isOn: $audio) {
+                        Text("Audio")
+                            .frame(maxWidth: .infinity, alignment: .trailing)
+                    }
+                }
+                    .padding(.horizontal, 10)
+
+                Divider()
+
+                Stepper(onIncrement: incrementStep,
+                    onDecrement: decrementStep) {
+                    Text("\(value) segundos")
+                }
+                    .padding(.horizontal, 80)
+
+                Divider()
+
+                Text("\(timeRemaining)")
+                    .onReceive(timer) { _ in
+                    if timeRemaining > 0 {
+                        timeRemaining -= 1
+                    } else {
+                        timeRemaining = value
+                        nuevoKana()
+                    }
+                }
+                    .font(.system(size: 36))
+                    .frame(width: 80, height: 80, alignment: .center)
+                    .foregroundColor(.accentColor)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(40)
             }
-                .pickerStyle(SegmentedPickerStyle())
-                .padding(.horizontal, 40)
-
-            VStack {
-                Text(etiqueta)
-                    .opacity(verKana ? 1 : 0)
-                    .font(.custom("Hiragino Mincho ProN W3", size: 144))
-                    .padding()
-                    .foregroundColor(Color(.label))
-                    .background(Color("Fondo"))
-                    .cornerRadius(10.0)
-
-                Text(romaji)
-                    .opacity(verRomaji ? 1 : 0)
-            }
-                .padding(.vertical, 20)
-
-            Divider()
-
+        } else {
             HStack {
-                Toggle(isOn: $verKana) {
-                    Text("Kana")
-                        .frame(maxWidth: .infinity, alignment: .trailing)
+                VStack {
+                    Text(etiqueta)
+                        .frame(minWidth: 300)
+                        .opacity(verKana ? 1 : 0)
+                        .font(.custom("Hiragino Mincho ProN W3", size: 144))
+                        .padding()
+                        .foregroundColor(Color(.label))
+                        .background(Color("Fondo"))
+                        .cornerRadius(10.0)
+
+                    Text(romaji)
+                        .opacity(verRomaji ? 1 : 0)
                 }
-                Toggle(isOn: $verRomaji) {
-                    Text("Romaji")
-                        .frame(maxWidth: .infinity, alignment: .trailing)
-                }
-                Toggle(isOn: $audio) {
-                    Text("Audio")
-                        .frame(maxWidth: .infinity, alignment: .trailing)
+                    .padding(.vertical, 20)
+
+                VStack {
+
+                    VStack {
+                        Picker("Silabario", selection: $silabarioSeleccionado) {
+                            ForEach(Silabario.allCases, id: \.self) { silabario in
+                                Text(silabario.rawValue.capitalized)
+                            }
+                        }
+
+                        Picker("Nivel", selection: $nivelSeleccionado) {
+                            Text("Básico").tag(Nivel.basico)
+                            Text("Ten-Ten").tag(Nivel.tenten)
+                            Text("Compuestos").tag(Nivel.compuestos)
+                        }
+                    }
+                        .pickerStyle(SegmentedPickerStyle())
+                        .padding(.horizontal, 40)
+
+                    Divider()
+
+                    HStack {
+                        Toggle(isOn: $verKana) {
+                            Text("Kana")
+                                .frame(maxWidth: .infinity, alignment: .trailing)
+                        }
+                        Toggle(isOn: $verRomaji) {
+                            Text("Romaji")
+                                .frame(maxWidth: .infinity, alignment: .trailing)
+                        }
+                        Toggle(isOn: $audio) {
+                            Text("Audio")
+                                .frame(maxWidth: .infinity, alignment: .trailing)
+                        }
+                    }
+                        .padding(.horizontal, 10)
+
+                    Divider()
+
+                    Stepper(onIncrement: incrementStep,
+                        onDecrement: decrementStep) {
+                        Text("\(value) segundos")
+                    }
+                        .padding(.horizontal, 80)
+
+                    Divider()
+
+                    Text("\(timeRemaining)")
+                        .onReceive(timer) { _ in
+                        if timeRemaining > 0 {
+                            timeRemaining -= 1
+                        } else {
+                            timeRemaining = value
+                            nuevoKana()
+                        }
+                    }
+                        .font(.system(size: 36))
+                        .frame(width: 80, height: 80, alignment: .center)
+                        .foregroundColor(.accentColor)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(40)
                 }
             }
-                .padding(.horizontal, 10)
-
-            Divider()
-
-            Stepper(onIncrement: incrementStep,
-                onDecrement: decrementStep) {
-                Text("\(value) segundos")
-            }
-                .padding(.horizontal, 80)
-
-            Divider()
-
-            Text("\(timeRemaining)")
-                .onReceive(timer) { _ in
-                if timeRemaining > 0 {
-                    timeRemaining -= 1
-                } else {
-                    timeRemaining = value
-                    nuevoKana()
-                }
-            }
-                .font(.system(size: 36))
-                .frame(width: 80, height: 80, alignment: .center)
-                .foregroundColor(.accentColor)
-                .background(Color(.systemGray6))
-                .cornerRadius(40)
-        }
-            .onAppear() {
-            nuevoKana()
         }
     }
 
