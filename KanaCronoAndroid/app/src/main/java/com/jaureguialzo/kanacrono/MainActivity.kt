41038fc3b3a65168c949cd8e38dfb0f51a507c3e
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2021-01-03 19:16:02 +0100

    Tener en cuenta las opciones

--- KanaCronoAndroid/app/src/main/java/com/jaureguialzo/kanacrono/MainActivity.kt
+++ KanaCronoAndroid/app/src/main/java/com/jaureguialzo/kanacrono/MainActivity.kt
@@ -2,6 +2,7 @@ package com.jaureguialzo.kanacrono
 
 import android.os.Bundle
 import android.view.View
+import android.widget.RadioGroup
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -28,8 +29,31 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun nuevoKana() {
-        val aleatorio = tuplaKana()
+        val aleatorio = tuplaKana(silabarioSeleccionado(), nivelSeleccionado())
         etiquetaKana.text = aleatorio.first
         etiquetaRomaji.text = aleatorio.second
+    }
+
+    fun silabarioSeleccionado(): Silabario {
+
+        val silabario = findViewById<RadioGroup>(R.id.silabarioRadioGroup).checkedRadioButtonId
+
+        return when (silabario) {
+            R.id.radioButtonHiragana -> Silabario.hiragana
+            R.id.radioButtonKatakana -> Silabario.katakana
+            else -> Silabario.hiragana
+        }
+    }
+
+    fun nivelSeleccionado(): Nivel {
+
+        val nivel = findViewById<RadioGroup>(R.id.nivelRadioGroup).checkedRadioButtonId
+
+        return when (nivel) {
+            R.id.radioButtonBasico -> Nivel.basico
+            R.id.radioButtonDiacriticos -> Nivel.tenten
+            R.id.radioButtonDigrafos -> Nivel.compuestos
+            else -> Nivel.basico
+        }
     }
 }
