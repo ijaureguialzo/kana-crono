9606066022a08d84bdcf24b2ed0ddfcd2a869705
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2021-01-04 12:01:09 +0100

    Cronómetro funcionando

--- KanaCronoAndroid/app/src/main/java/com/jaureguialzo/kanacrono/MainActivity.kt
+++ KanaCronoAndroid/app/src/main/java/com/jaureguialzo/kanacrono/MainActivity.kt
@@ -2,10 +2,12 @@ package com.jaureguialzo.kanacrono
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageButton
 import android.widget.RadioGroup
 import android.widget.Switch
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,10 +16,16 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var etiquetaKana: TextView
     private lateinit var etiquetaRomaji: TextView
+    private lateinit var etiquetaSegundos: TextView
     private lateinit var silabario: RadioGroup
     private lateinit var nivel: RadioGroup
     private lateinit var verKana: Switch
     private lateinit var verRomaji: Switch
+
+    private lateinit var timer: Timer
+    private var segundos = 5
+    private var timeRemaining = 5
+    private var timerRunning = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +33,7 @@ class MainActivity : AppCompatActivity() {
 
         etiquetaKana = findViewById(R.id.etiquetaKana)
         etiquetaRomaji = findViewById(R.id.etiquetaRomaji)
+        etiquetaSegundos = findViewById(R.id.etiquetaSegundos)
         silabario = findViewById(R.id.silabarioRadioGroup)
         nivel = findViewById(R.id.nivelRadioGroup)
         verKana = findViewById(R.id.switchKana)
@@ -46,10 +55,63 @@ class MainActivity : AppCompatActivity() {
             else
                 etiquetaRomaji.alpha = 0.0F
         }
+
+        timeRemaining = segundos
+        iniciarReloj()
+    }
+
+    fun iniciarReloj() {
+
+        etiquetaSegundos.text = "${timeRemaining}"
+
+        timer = Timer()
+
+        // REF: https://stackoverflow.com/a/46144839
+        timer.schedule(object : TimerTask() {
+            override fun run() {
+                runOnUiThread(object : TimerTask() {
+                    override fun run() {
+                        if (timerRunning) {
+                            if (timeRemaining > 0) {
+                                timeRemaining -= 1
+                            } else {
+                                timeRemaining = segundos
+                            }
+                            etiquetaSegundos.text = "${timeRemaining}"
+                        }
+                    }
+                })
+            }
+        }, 1000, 1000)
+    }
+
+    fun pararReloj() {
+        timer.cancel()
     }
 
     fun botonSiguiente(view: View) {
+        timeRemaining = segundos
+        etiquetaSegundos.text = "${timeRemaining}"
         nuevoKana()
+
+        if (timerRunning) {
+            pararReloj()
+            iniciarReloj()
+        }
+    }
+
+    fun botonPausa(view: View) {
+        val boton = view as ImageButton
+
+        if (timerRunning) {
+            pararReloj()
+            timerRunning = false
+            boton.setImageResource(android.R.drawable.ic_media_play)
+        } else {
+            iniciarReloj()
+            timerRunning = true
+            boton.setImageResource(android.R.drawable.ic_media_pause)
+        }
     }
 
     fun nuevoKana() {
