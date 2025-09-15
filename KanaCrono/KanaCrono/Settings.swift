0c1ae3aa62d424392e1e1edeccc57b85a7b6fc7c
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2025-09-15 09:50:21 +0200

    Página de ajustes revisada para iOS 26

--- KanaCrono/KanaCrono/Settings.swift
+++ KanaCrono/KanaCrono/Settings.swift
@@ -15,29 +15,34 @@ struct Settings: View {
 
         NavigationView {
 
-            List {
-                Section(header: Text("SETTINGS_SYLLABARY")) {
-                    Selectores()
+            // Parche para iOS 26: https://developer.apple.com/forums/thread/787893
+            VStack(spacing: 0) {
+                Color(.systemGroupedBackground).frame(height: 16)
+                List {
+                    Section(header: Text("SETTINGS_SYLLABARY")) {
+                        Selectores()
+                    }
+                    Section(header: Text("SETTINGS_FONT")) {
+                        SelectorFuente()
+                    }
+                    Section(header: Text("SETTINGS_VISIBILITY")) {
+                        OpcionesVisibilidad()
+                    }
+                    Section(header: Text("SETTINGS_TIME")) {
+                        StepperSegundos()
+                    }
                 }
-                Section(header: Text("SETTINGS_FONT")) {
-                    SelectorFuente()
-                }
-                Section(header: Text("SETTINGS_VISIBILITY")) {
-                    OpcionesVisibilidad()
-                }
-                Section(header: Text("SETTINGS_TIME")) {
-                    StepperSegundos()
-                }
-            }.listStyle(.plain)
-                .navigationBarTitle("SETTINGS_TITLE")
-                .navigationBarItems(trailing: Button(action: {
-                self.presentationMode.wrappedValue.dismiss()
-            }) {
-                    Text(Image(systemName: "xmark.circle.fill"))
-                        .foregroundColor(Color(UIColor.lightGray))
-                        .opacity(0.33)
-                })
-
+                    .listStyle(.grouped)
+                    .navigationBarTitle("SETTINGS_TITLE")
+                    .navigationBarItems(trailing: Button(action: { self.presentationMode.wrappedValue.dismiss() }) {
+                        if #unavailable(iOS 26.0) {
+                            Text(Image(systemName: "xmark.circle.fill"))
+                                .foregroundColor(Color(.tertiaryLabel))
+                        } else {
+                            Text(Image(systemName: "xmark"))
+                        }
+                    })
+            }
         }
     }
 }
